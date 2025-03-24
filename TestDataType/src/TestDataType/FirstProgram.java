46012535b687817ad5ee822baa5ba35d088d248d
Md. Imran Mia <mr.t.imran11@gmail.com>
2025-03-24 14:31:55 +0600

    add

---
+++ TestDataType/src/TestDataType/FirstProgram.java
@@ -0,0 +1,22 @@
+
+package TestDataType;
+
+
+public class FirstProgram {
+
+    
+     public static void main(String[] args) {
+        byte id=120;
+        short roll=12145;
+        int number=1366546;
+        long longNumber=134665465465l;
+        String userName ="Imran";
+        char latter='M';
+        boolean status = true;
+        
+        System.out.println("Your optimum latter is "+latter+" User Name "+userName+" id no "+id);
+       
+        
+    }
+    
+}
