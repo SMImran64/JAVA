76b5b472ebda1c34fd3a0ac1edda489f0e8fbc0f
Md. Imran Mia <mr.t.imran11@gmail.com>
2025-04-05 18:53:14 +0600

    add

--- ConvertSecondToMinutes/src/convertsecondtominutes/ConvertSecondToMinutes.java
+++ ConvertSecondToMinutes/src/convertsecondtominutes/ConvertSecondToMinutes.java
@@ -9,22 +9,38 @@ public class ConvertSecondToMinutes {
 
     
     public static void main(String[] args) {
+        
+//        Scanner scanner = new Scanner(System.in);
+//        
+//        System.out.println("Enter Second");  
+//        int totalSeconds = scanner.nextInt();
+//       
+//        int hours = totalSeconds/3600;  
+//        
+//        int remainingSecond = totalSeconds%3600;
+//        
+//        int minutes= remainingSecond/60;
+//        
+//        int seconds= remainingSecond%60;
+//        
+//        
+//        System.out.println(totalSeconds+" seconds= "+hours+" hours ,"+minutes
+//                +" Minutes "+ seconds + "seconds");
+
         Scanner scanner = new Scanner(System.in);
         
-        System.out.println("Enter Second");  
-        int totalSeconds = scanner.nextInt();
-       
-        int hours = totalSeconds/3600;
+        System.out.println("Enter Sencond");
         
+        int totalSeconds = scanner.nextInt();
+        
+        int hours = totalSeconds/3600;
         int remainingSecond = totalSeconds%3600;
         
-        int minutes= remainingSecond/60;
+        int minutes = remainingSecond/60;
         
-        int seconds= remainingSecond%60;
-        
-        
-        System.out.println(totalSeconds+" seconds= "+hours+" hours ,"+minutes
-                +" Minutes "+ seconds + "seconds");
+       int seconds = remainingSecond%60;
+       
+        System.out.println(hours +" hours ,"+minutes +" Minutes ,"+ seconds+ "seconds");
         
         
         
