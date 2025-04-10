4f10056afcc0ecfe5939263f78b098f6a538af6a
Md. Imran Mia <mr.t.imran11@gmail.com>
2025-04-11 00:24:47 +0600

    add

--- FactorialNumberInStaticMethod/src/factorialnumberinstaticmethod/FactorialNumberInStaticMethod.java
+++ FactorialNumberInStaticMethod/src/factorialnumberinstaticmethod/FactorialNumberInStaticMethod.java
@@ -1,53 +1,45 @@
-
 package factorialnumberinstaticmethod;
 
-import com.sun.source.tree.BreakTree;
 import java.util.Scanner;
 
-
 public class FactorialNumberInStaticMethod {
-    
-    
 
-  
     public static void main(String[] args) {
         Scanner f = new Scanner(System.in);
-        
-        System.out.println("Enter value");
-        
-        int value = f.nextInt();
-        
-        String result = FactorialNumberInStaticMethod(value);
-        
-        System.out.println(result);
-    
-        }
-    public static String FactorialNumberInStaticMethod(int value){
-    
-        String result = "";
-        
-        long factorial = 1;
-           int count = 1;
-        
-        if (value < 0) {
-            
-            
-            System.err.println("Mathmatical Erorr");
-            System.exit(0);
-        } else {
 
+        System.out.println("Enter value");
+
+        int value = f.nextInt();
+
+        String result = factorialNumberInStaticMethod(value);
+
+        System.out.println(result);
+
+    }
+
+    public static String factorialNumberInStaticMethod(int value) {
+
+        String result = "";
+
+        long factorial = 1;
+        int count = 1;
+
+        if (value <= 0) {
+
+            result = "Mathmatical Erorr";
+
+        } else {
             do {
-                factorial *= count;
+                long givenResult = factorial *= count;
                 count++;
+                result = "The Factorial Number is " + givenResult;
 
             } while (count <= value);
 
         }
 
-       result = "The Factorial Number is: " + factorial;
+//       result = "The Factorial Number is: " + factorial;
+        return result;
 
-    return result;
-    
     }
-}           
-
+}
