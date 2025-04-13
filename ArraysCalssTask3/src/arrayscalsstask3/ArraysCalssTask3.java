596f9f24e1e2c2780a8fd5c9fe4e6bb830746bef
Md. Imran Mia <mr.t.imran11@gmail.com>
2025-04-14 00:02:41 +0600

    casting

--- ArraysCalssTask3/src/arrayscalsstask3/ArraysCalssTask3.java
+++ ArraysCalssTask3/src/arrayscalsstask3/ArraysCalssTask3.java
@@ -1,40 +1,42 @@
-
 package arrayscalsstask3;
 
 import java.util.Scanner;
 
 public class ArraysCalssTask3 {
 
-   
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        
-        System.out.println("Enter limit");
-        
-        int n = s.nextInt();
-        
-        String[]names = new String[n];
-        
-        int []passwords = new int[n];
-        
-        for(int i = 0; i<names.length; i++){
-            
-            System.out.println("Enter your name");
-            
-            String name = s.next();
-            
-            names[i]=name;
-            
-            if( names[i].equalsIgnoreCase(name)){
-                
-                
-             
-                
-            
+
+        System.out.println("Enter Your Name");
+
+        String userName = s.next();
+
+        String[] names = {"Imran", "Hujaifa", "Jawad", "Abdullah", "Iyat"};
+
+        String[] passwords = {"i143", "h243", "j543", "a643", "443"};
+
+        for (int i = 0; i < names.length; i++) {
+
+            String pass = "";
+
+            if (userName.equalsIgnoreCase(names[i])) {
+
+                System.out.println("Enter your password ");
+
+                pass = s.next();
+
+                if (pass.equals(passwords[i])) {
+                    System.out.println("Assalamualikum " + userName.toUpperCase());
+                } else {
+                    System.out.println("Invalid Passwords");
+                }
+                break;
+            } else {
+                System.out.println("Invalid UserNames ");
             }
+
         }
-        
-        
+
     }
-    
+
 }
