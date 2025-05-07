9cae6ec010c29d06aefce43cc590a26675d7cb7e
Md. Imran Mia <mr.t.imran11@gmail.com>
2025-05-07 13:29:31 +0600

    Map

--- MapsMethodLearn/src/mapsmethodlearn/MapsMethodLearn.java
+++ MapsMethodLearn/src/mapsmethodlearn/MapsMethodLearn.java
@@ -2,19 +2,43 @@ package mapsmethodlearn;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class MapsMethodLearn {
 
     public static void main(String[] args) {
-
-        Map<Integer, StudentClassRound_64_Java> imran = new HashMap<>();
-
-        imran.put(1, new StudentClassRound_64_Java(1287829, "imran", "imran@gamil.com", "Azimpur "+"\n"));
-        imran.put(2, new StudentClassRound_64_Java(1287829, "masud", "masud@gamil.com", "Azimpur"));
-        imran.put(3, new StudentClassRound_64_Java(1287829, "Istiaque", "istiaque@gamil.com", "Azimpur"));
-        imran.put(4, new StudentClassRound_64_Java(1287829, "Parvez", "parvez@gamil.com", "Azimpur"));
-
-        System.out.println(imran);
+        
+        try {
+            Scanner s = new Scanner(System.in);
+            System.out.println("Enter your Key Number : ");
+            
+            int studentId = s.nextInt();
+            
+            Map<Integer,StudentClassRound_64_Java> stDetails = new HashMap<>();
+            
+            stDetails.put(1001,new StudentClassRound_64_Java(1287829, "Md. Imran Mia ", "mr.t.imran11@gamil.com", "Java", "CCSl", "Md. Emran Hossain", "Azimpur"));
+            stDetails.put(1002,new StudentClassRound_64_Java(1287462, "Abdullah-Al Masud ", "mr.t.imran11@gamil.com", "Java", "CCSl", "Md. Emran Hossain", "Khilgawn"));
+            stDetails.put(1003,new StudentClassRound_64_Java(1287748, "Samim Junayet Istique ", "mr.t.imran11@gamil.com", "Java", "CCSl", "Md. Emran Hossain", "Khilgawn"));
+            stDetails.put(1004,new StudentClassRound_64_Java(1287271, "Md. Atiqur Rahman ", "mr.t.imran11@gamil.com", "Java", "CCSl", "Md. Emran Hossain", "Khilgawn"));
+            stDetails.put(1005,new StudentClassRound_64_Java(1287759, "CM Rejaul Karim ", "mr.t.imran11@gamil.com", "Java", "CCSl", "Md. Emran Hossain", "Khilgawn"));
+            stDetails.put(1006,new StudentClassRound_64_Java(1287470, "Md. Eiasin Prodhan ", "mr.t.imran11@gamil.com", "Java", "CCSl", "Md. Emran Hossain", "Khilgawn"));
+            stDetails.put(1007,new StudentClassRound_64_Java(1287799, "Md. Parvej Hossen ", "mr.t.imran11@gamil.com", "Java", "CCSl", "Md. Emran Hossain", "Khilgawn"));
+            stDetails.put(1008,new StudentClassRound_64_Java(1287675, "Md. Rakib Islam ", "mr.t.imran11@gamil.com", "Java", "CCSl", "Md. Emran Hossain", "Khilgawn"));
+            stDetails.put(1009,new StudentClassRound_64_Java(1287315, "Mrs. Sathe Khanom ", "mr.t.imran11@gamil.com", "Java", "CCSl", "Md. Emran Hossain", "Khilgawn"));
+            stDetails.put(1010,new StudentClassRound_64_Java(1287519, "Md. RahmatUllah Shahrukh ", "mr.t.imran11@gamil.com", "Java", "CCSl", "Md. Emran Hossain", "Khilgawn"));
+            stDetails.put(1011,new StudentClassRound_64_Java(1287829, "Md. Imran Mia ", "mr.t.imran11@gamil.com", "Java", "CCSl", "Md. Emran Hossain", "Khilgawn"));
+            stDetails.put(1012,new StudentClassRound_64_Java(1287647, "Md. Sadiar Rahman ", "mr.t.imran11@gamil.com", "Java", "CCSl", "Md. Emran Hossain", "Khilgawn"));
+            stDetails.put(1013,new StudentClassRound_64_Java(1287039, "Md. Atiqul Islam ", "mr.t.imran11@gamil.com", "Java", "CCSl", "Md. Emran Hossain", "Khilgawn"));
+            stDetails.put(1014,new StudentClassRound_64_Java(1287519, "Mrs. halima Akter ", "mr.t.imran11@gamil.com", "Java", "CCSl", "Md. Emran Hossain", "Khilgawn"));
+            
+            System.out.println("Student Information are given bellow");
+            
+            System.out.println(stDetails.get(studentId).toString());
+            
+        } catch (Exception e) {
+            
+            System.out.println("Invalid id Number!! Please enter your correct Id ");
+        }
           
     }
 
--- MapsMethodLearn/src/mapsmethodlearn/StudentClassRound_64_Java.java
+++ MapsMethodLearn/src/mapsmethodlearn/StudentClassRound_64_Java.java
@@ -7,17 +7,33 @@ public class StudentClassRound_64_Java {
     protected int studentId;
     protected String name;
     protected String email;
+    protected String courseTitle;
+    protected String center;
+    protected String teacher;
     protected String address;
 
     public StudentClassRound_64_Java() {
     }
 
-    public StudentClassRound_64_Java(int studentId, String name, String email, String address) {
+    public StudentClassRound_64_Java(int studentId, String name, String email, String courseTitle, String center, String teacher, String address) {
         this.studentId = studentId;
         this.name = name;
         this.email = email;
+        this.courseTitle = courseTitle;
+        this.center = center;
+        this.teacher = teacher;
         this.address = address;
     }
+
+    public String getCourseTitle() {
+        return courseTitle;
+    }
+
+    public void setCourseTitle(String courseTitle) {
+        this.courseTitle = courseTitle;
+    }
+
+    
 
     public int getStudentId() {
         return studentId;
@@ -43,6 +59,22 @@ public class StudentClassRound_64_Java {
         this.email = email;
     }
 
+    public String getCenter() {
+        return center;
+    }
+
+    public void setCenter(String center) {
+        this.center = center;
+    }
+
+    public String getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(String teacher) {
+        this.teacher = teacher;
+    }
+
     public String getAddress() {
         return address;
     }
@@ -52,9 +84,13 @@ public class StudentClassRound_64_Java {
     }
     
 
+   
+
     @Override
     public String toString() {
-        return  "\n \n StudentId: " + studentId + "\n Name: " + name + "\n Email Address: " + email + "\n Address: " + address;
+        return  "\n \n StudentId: " + studentId + "\n Name: " + name +
+        "\n Email Address: " + email +"\n Course Title: "+courseTitle+
+        "\nClass Constructor: "+teacher+"Course Center: "+center+ "\n Address: " + address;
     }
     
     
