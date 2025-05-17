626facfbe620d200bf931b8097d7a239fd98ab9f
Md. Imran Mia <mr.t.imran11@gmail.com>
2025-05-17 14:26:23 +0600

    add

--- SqlTest/src/sqltest/SqlTest.java
+++ SqlTest/src/sqltest/SqlTest.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class SqlTest {
+public class SqlTest { 
     
     public static void main(String[] args) {
         
