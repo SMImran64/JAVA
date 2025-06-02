676088c45cbe74b2ad8c7b9e1c0214b2b7e12c1d
Md. Imran Mia <mr.t.imran11@gmail.com>
2025-06-02 18:57:29 +0600

    add

--- PointOfSell/src/pointofsell/PointOfSell.java
+++ PointOfSell/src/pointofsell/PointOfSell.java
@@ -1,11 +1,17 @@
 
 package pointofsell;
 
+import pointofsell.View.Login;
+
 
 public class PointOfSell {
 
     
     public static void main(String[] args) {
+        
+        Login l = new Login();
+        
+        l.setVisible(true);
        
     }
     
